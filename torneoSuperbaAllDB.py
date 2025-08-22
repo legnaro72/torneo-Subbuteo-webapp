d0a1cfad16619c76c717e6b25d808d6c31fb81aa
legnaro72 <massimiliano.ferrando@gmail.com>
2025-08-23 01:48:44 +0200

    Update torneoSuperbaAllDB.py

--- torneoSuperbaAllDB.py
+++ torneoSuperbaAllDB.py
@@ -13,10 +13,6 @@ import time
 
 st.write("Chiavi disponibili nei secrets:", list(st.secrets.keys()))
 
-# -------------------------
-# Connessione a MongoDB Atlas
-# -------------------------
-
 # Variabili globali per i DB
 players_client=None
 tournaments_client=None
@@ -25,6 +21,27 @@ tournaments_collection=None
 players_db=None
 tournaments_db=None
 
+# -------------------------
+# Connessione a MongoDB Atlas
+# -------------------------
+
+players_collection = None
+st.info("Tentativo di connessione a MongoDB...")
+try:
+    MONGO_URI = st.secrets["MONGO_URI"]
+    server_api = ServerApi('1')
+    client = MongoClient(MONGO_URI, server_api=server_api)
+    
+    # Ho corretto il nome del database e della collection
+    db = client.get_database("giocatori_subbuteo")
+    players_collection = db.get_collection("superba_players") 
+
+    _ = players_collection.find_one()
+    st.success("✅ Connessione a MongoDB Atlas riuscita per la lettura dei giocatori.")
+except Exception as e:
+    st.error(f"❌ Errore di connessione a MongoDB: {e}. Non sarà possibile caricare i giocatori dal database.")# -------------------------
+# Connessione a MongoDB Atlas
+# -----------
 st.info("Tentativo di connessione ai database...")
 
 try:
