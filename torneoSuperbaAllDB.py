bd8103718eddcdd3b89c6c4c3daa72c23d74c6ff
legnaro72 <massimiliano.ferrando@gmail.com>
2025-08-23 13:05:52 +0200

    Update torneoSuperbaAllDB.py

--- torneoSuperbaAllDB.py
+++ torneoSuperbaAllDB.py
@@ -28,25 +28,13 @@ try:
 
     _ = players_collection.find_one()
     st.success("✅ Connessione a MongoDB Atlas (giocatori) riuscita.")
-except Exception as e:
-    st.error(f"❌ Errore di connessione a MongoDB (giocatori): {e}")
-
-
-# -------------------------
-# Connessione a MongoDB Atlas - Tournaments
-# -------------------------
-
-tournaments_collection = None
-st.info("Tentativo di connessione a MongoDB (tournaments)...")
-
-try:
-    MONGO_URI_TOURNAMENTS=st.secrets["MONGO_URI_TOURNAMENTS"]
-    client_tournaments=MongoClient(MONGO_URI_TOURNAMENTS, server_api=server_api)
 
     db_tournaments=client_tournaments.get_database("subbuteo_tournement")
     tournaments_collection=db_tournaments.get_collection("tournement")
 
     _ = tournaments_collection.find_one()
-    st.success("✅ Connessione a MongoDB Atlas (tournaments) riuscita.")
+
 except Exception as e:
-    st.error(f"❌ Errore di connessione a MongoDB (tournaments): {e}")
+    st.error(f"❌ Errore di connessione a MongoDB (giocatori): {e}")
+
+
