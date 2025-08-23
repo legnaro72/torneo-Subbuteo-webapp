ad91aa828a7d250b9e21240e9c6c0d2180beb01e
legnaro72 <massimiliano.ferrando@gmail.com>
2025-08-23 13:11:17 +0200

    Update torneoSuperbaAllDB.py

--- torneoSuperbaAllDB.py
+++ torneoSuperbaAllDB.py
@@ -7,22 +7,17 @@ st.title("Test connessione MongoDB")
 # Debug: mostra chiavi disponibili nei secrets
 st.write("Chiavi disponibili nei secrets:", list(st.secrets.keys()))
 
-import streamlit as st
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-
 # -------------------------
 # Connessione a MongoDB Atlas - Giocatori
 # -------------------------
 
 players_collection = None
 tournaments_collection = None
-
-st.info("Tentativo di connessione a MongoDB ...")
+server_api = ServerApi('1')  # riusabile per entrambi
 
 try:
-    MONGO_URI = st.secrets["MONGO_URI"]  # 🔑 preso da .streamlit/secrets.toml
-    server_api = ServerApi('1')
+    # Connessione Giocatori
+    MONGO_URI = st.secrets["MONGO_URI"]
     client_players = MongoClient(MONGO_URI, server_api=server_api)
 
     db_players = client_players.get_database("giocatori_subbuteo")
@@ -31,12 +26,24 @@ try:
     _ = players_collection.find_one()
     st.success("✅ Connessione a MongoDB Atlas (giocatori) riuscita.")
 
-    db_tournaments = client_tournaments.get_database("subbuteo_tournement")
-    tournaments_collection = db_tournaments.get_collection("tournement")
-
-    _ = tournaments_collection.find_one()
-
 except Exception as e:
     st.error(f"❌ Errore di connessione a MongoDB (giocatori): {e}")
 
 
+# -------------------------
+# Connessione a MongoDB Atlas - Tournaments
+# -------------------------
+
+try:
+    # Connessione Tournaments
+    MONGO_URI_TOURNAMENTS = st.secrets["MONGO_URI_TOURNAMENTS"]
+    client_tournaments = MongoClient(MONGO_URI_TOURNAMENTS, server_api=server_api)
+
+    db_tournaments = client_tournaments.get_database("subbuteo_tournament")
+    tournaments_collection = db_tournaments.get_collection("tournament")
+
+    _ = tournaments_collection.find_one()
+    st.success("✅ Connessione a MongoDB Atlas (tournaments) riuscita.")
+
+except Exception as e:
+    st.error(f"❌ Errore di connessione a MongoDB (tournaments): {e}")
