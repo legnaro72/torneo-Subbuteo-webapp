9eacee893675f3d834b85b48d6dd6f6cc0b7741e
legnaro72 <massimiliano.ferrando@gmail.com>
2025-08-24 22:38:09 +0200

    Update torneoSuperbaAllDB.py

--- torneoSuperbaAllDB.py
+++ torneoSuperbaAllDB.py
@@ -293,9 +293,9 @@ def salva_risultati_giornata(tournaments_collection, girone_sel, giornata_sel):
     st.rerun()
 
 def mostra_classifica_stilizzata(df_classifica, girone_sel):
-    # DEBUG
-    st.write("DEBUG classifica:", type(df_classifica))
-    st.write(df_classifica)
+    # DEBUG: mostra tipo e prime righe
+    st.write("DEBUG tipo classifica:", type(df_classifica))
+    st.write("DEBUG contenuto classifica:", df_classifica.head())
 
     if df_classifica is None:
         st.info("⚽ Nessuna classifica disponibile")
