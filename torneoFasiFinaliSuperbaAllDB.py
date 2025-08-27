cc31a58cd5ed478ab5e3c526b65a4882bf278446
legnaro72 <massimiliano.ferrando@gmail.com>
2025-08-27 15:14:54 +0200

    Update torneoFasiFinaliSuperbaAllDB.py

--- torneoFasiFinaliSuperbaAllDB.py
+++ torneoFasiFinaliSuperbaAllDB.py
@@ -514,9 +514,9 @@ if st.session_state['ui_show_pre']:
     if not uri:
         st.error("Variabile d'ambiente MONGODB_URI non impostata.")
     
-    db_name = "TORNEI SUBUTEO LA COLLECTION SUPERBA"
-    col_name = "tournaments"
-    tournaments_collection = init_mongo_connection(uri, db_name, col_name)
+    db_name = "TorneiSubbuteo"
+    col_name = "Superba"
+    tournaments_collection = init_mongo_connection(st.secrets["MONGO_URI_TOURNEMENTS"], "TorneiSubbuteo", "Superba", show_ok=False)
     
     if tournaments_collection:
         tornei_trovati = carica_tornei_da_db(tournaments_collection, prefix="COMPLETATO UNDERSCORE")
