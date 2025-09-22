b1b2cedcf844d2243b0fe7b6a30394c3153f8dd8
legnaro72 <massimiliano.ferrando@gmail.com>
2025-09-22 18:58:39 +0200

    Update TorneoSubbuteoSvizzeroSuperbaAllDBNewVersion.py

--- TorneoSubbuteoSvizzeroSuperbaAllDBNewVersion.py
+++ TorneoSubbuteoSvizzeroSuperbaAllDBNewVersion.py
@@ -1758,11 +1758,14 @@ if st.session_state.torneo_finito:
         with placeholder.container():
             st.balloons()
             time.sleep(1) # Aspetta 1 secondo
+
+# Inizializza il keep-alive
+add_keep_alive()
+
 # Footer leggero
 st.markdown("---")
 st.caption("⚽ Subbuteo Tournament Manager •  Made by Legnaro72")
 
-# Inizializza il keep-alive
-#add_keep_alive()
+
 
 # Non è necessario il blocco if __name__ == "__main__" in un'app Streamlit
