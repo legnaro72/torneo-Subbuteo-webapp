bda4c7964d7cff5add07956bfc7e68b651623649
legnaro72 <massimiliano.ferrando@gmail.com>
2025-09-25 00:38:20 +0200

    Update hubTorneiSubbuteoTigullioAllDB.py

--- hubTorneiSubbuteoTigullioAllDB.py
+++ hubTorneiSubbuteoTigullioAllDB.py
@@ -156,7 +156,7 @@ with col3:
             </div>
             <div class="card-link-container">
                 <a class="card-link" href="https://torneo-subbuteo-tigullio-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App</a>
-                #<a class="card-link card-link-beta" href="https://torneo-subbuteo-tigullio-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App Beta ++</a>
+                <a class="card-link card-link-beta" href="https://torneo-subbuteo-tigullio-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App Beta ++</a>
             </div>
         </div>
     ''', unsafe_allow_html=True)
