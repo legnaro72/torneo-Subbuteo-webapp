934257f612a2af2ae9a705c4d31e76d5ea2af5d5
legnaro72 <massimiliano.ferrando@gmail.com>
2025-09-24 18:52:12 +0200

    Update hubTorneiSubbuteoSuperbaAllDB.py

--- hubTorneiSubbuteoSuperbaAllDB.py
+++ hubTorneiSubbuteoSuperbaAllDB.py
@@ -155,8 +155,8 @@ with col3:
                 competizioni equilibrate tra più squadre/club.
             </div>
             <div class="card-link-container">
-                <a class="card-link" href="https://torneo-subbuteo-superba-svizzero-alldb.streamlit.app/" target="_blank">Apri App</a>
-                <a class="card-link card-link-beta" href="https://torneo-subbuteo-superba-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App Beta ++</a>
+                <a class="card-link" href="https://torneo-subbuteo-superba-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App</a>
+                #<a class="card-link card-link-beta" href="https://torneo-subbuteo-superba-new-version-svizzero-alldb.streamlit.app/" target="_blank">Apri App Beta ++</a>
             </div>
         </div>
     ''', unsafe_allow_html=True)
