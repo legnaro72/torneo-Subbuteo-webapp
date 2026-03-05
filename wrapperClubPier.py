2d3d7c029e7ffa95378b96c7abde9bce9e60d47e
legnaro72 <massimiliano.ferrando@gmail.com>
2026-03-05 22:35:58 +0100

    Add files via upload

--- wrapperClubPier.py
+++ wrapperClubPier.py
@@ -1,4 +1,7 @@
 import runpy
 
-# esegue lo script originale come se fosse main
-runpy.run_path("editPierCrewClubAllDBNew.py")
+# Parametri cablati per il club Pier Crew
+HUB_URL = "https://farm-tornei-subbuteo-piercrew-all-db.streamlit.app/"
+
+# Esegue lo script passandogli il contesto necessario (Pier Crew)
+runpy.run_path("editPierCrewClubAllDBNew.py", init_globals={"HUB_URL": HUB_URL, "CLUB_NAME": "Pier Crew"})
